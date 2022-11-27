35c83b5bb4e32bc4f3eaca6f1a6b08241702ec1c
AlifSakib <alifsakib@gmail.com>
2022-11-28 03:21:16 +0600

    banner section changed

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
-import banner_image from "../../assets/images/banner-image.avif";
+import image from "../../assets/images/banner-image.avif";
+import image2 from "../../assets/images/banner.jpg";
 
 const Banner = () => {
   return (
     <div>
-      <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
+      {/* <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
         <div className="w-full lg:w-1/2">
           <div className="lg:max-w-lg">
             <h1 className="text-3xl font-bold tracking-wide text-gray-800 dark:text-white lg:text-5xl">
@@ -74,6 +75,42 @@ const Banner = () => {
             src={banner_image}
             alt=""
           />
+        </div>
+      </div> */}
+
+      <div className="relative mb-4">
+        <img
+          src={image2}
+          className="absolute inset-0 object-cover w-full h-full"
+          alt=""
+        />
+        <div className="relative bg-opacity-75 bg-slate-900">
+          <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="flex flex-col items-center justify-between xl:flex-row">
+              <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+                <h2 className="max-w-lg mb-6 font-poppins text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                  Get the best of Shopping with LozzeBy.{" "}
+                  <br className="hidden md:block" />
+                  Enjoy low prices and great deals ...
+                </h2>
+                <p className="max-w-xl mb-4 text-base text-white font-poppins md:text-lg">
+                  LozzyBy a resale product based platfrom where you will find
+                  thousends of reused products in a reasonable price . The great
+                  thing is you can also become a member by creating and account
+                  today and its easy .
+                </p>
+              </div>
+              <div className="w-full max-w-2xl xl:px-8 ">
+                <div className="flex items-center justify-center w-full h-96 ">
+                  <img
+                    className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl"
+                    src={image}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Home/Cta.js
+++ src/components/Home/Cta.js
@@ -1,4 +1,5 @@
 import React from "react";
+import image from "../../assets/images/image1.jpg";
 
 const Cta = () => {
   return (
@@ -8,42 +9,40 @@ const Cta = () => {
           <div className="flex flex-col justify-center">
             <div className="max-w-xl mb-6">
               <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                The quick, brown fox
+                There are people who don't like capitalism,
                 <br className="hidden md:block" />
-                jumps over{" "}
+                and people{" "}
                 <span className="relative px-1">
                   <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
                   <span className="relative inline-block text-deep-purple-accent-400">
-                    a lazy dog
+                    who don't like PCs.
                   </span>
                 </span>
               </h2>
               <p className="text-base text-gray-700 md:text-lg">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                quae. explicabo.
+                You’ve got to start with the customer experience and work back
+                toward the technology
               </p>
             </div>
             <div className="grid gap-5 row-gap-8 sm:grid-cols-2">
               <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
                 <div className="h-full p-5 border border-l-0 rounded-r">
                   <h6 className="mb-2 font-semibold leading-5">
-                    I'll be sure to note that in my log
+                    We provide you trust and security
                   </h6>
                   <p className="text-sm text-gray-900">
-                    Lookout flogging bilge rat main sheet bilge water nipper
-                    fluke to go on account heave down.
+                    Become a seller today and start uploading your unused
+                    product
                   </p>
                 </div>
               </div>
               <div className="bg-white border-l-4 shadow-sm border-deep-purple-accent-400">
                 <div className="h-full p-5 border border-l-0 rounded-r">
                   <h6 className="mb-2 font-semibold leading-5">
-                    A business big enough that it could be listed
+                    We provide you trust and security
                   </h6>
                   <p className="text-sm text-gray-900">
-                    Those options are already baked in with this model shoot me
-                    an email clear.
+                    We care about our members
                   </p>
                 </div>
               </div>
@@ -52,7 +51,7 @@ const Cta = () => {
           <div>
             <img
               className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
-              src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+              src={image}
               alt=""
             />
           </div>
